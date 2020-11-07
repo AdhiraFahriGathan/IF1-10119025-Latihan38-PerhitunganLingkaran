91088164b67d15967f11916fbc89952b990d4738
unknown <adhiragathan12@gmail.com>
2020-11-07 09:02:17 +0700

    initial commit

--- src/if1/pkg10119025/latihan38/perhitunganlingkaran/Latihan38PerhitunganLingkaran.java
+++ src/if1/pkg10119025/latihan38/perhitunganlingkaran/Latihan38PerhitunganLingkaran.java
@@ -10,6 +10,10 @@ import java.util.Scanner;
 /**
  *
  * @author User
+ * * NAMA     : ADHIRA FAHRI GATHAN
+ * KELAS    : PBO
+ * NIM      : 10119025
+ * Deskripsi Program : Program ini berisi tentang Perhitungan Lingkaran
  */
 public class Latihan38PerhitunganLingkaran {
     static Scanner scan = new Scanner(System.in);
